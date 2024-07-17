de5efe7a345810b5e2c0bff9e42640da204f2e18
Santiago Espindola <santiago2211ar@hotmail.com>
2024-07-17 18:50:06 -0300

    I added indexed search to the contacts section

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,14 +3,64 @@ import { Link } from 'react-router-dom';
 
 const Chats = () => {
   const [hoveredChatId, setHoveredChatId] = useState(null);
+
   const [chats, setChats] = useState([
-    { id: 1, name: 'Santiago A', message: 'Lorem ipsum dolor sit amet', pinned: true, muted: false, newMsg: 1 },
-    { id: 2, name: 'Santiago B', message: 'Lorem ipsum dolor sit amet', pinned: false, muted: true, newMsg: 3 },
-    { id: 3, name: 'Santiago C', message: 'Lorem ipsum dolor sit amet', pinned: false, muted: false, newMsg: 0 },
-    { id: 4, name: 'Santiago D', message: 'Lorem ipsum dolor sit amet', pinned: false, muted: false, newMsg: 0 },
-    { id: 5, name: 'Santiago E', message: 'Lorem ipsum dolor sit amet', pinned: false, muted: false, newMsg: 0 },
-    { id: 6, name: 'Santiago F', message: 'Lorem ipsum dolor sit amet', pinned: false, muted: false, newMsg: 2 },
+    {
+      id: 1,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago A',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: true,
+      muted: false,
+      newMsg: 1,
+    },
+    {
+      id: 2,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago B',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: true,
+      newMsg: 3,
+    },
+    {
+      id: 3,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago C',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 0,
+    },
+    {
+      id: 4,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago D',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 0,
+    },
+    {
+      id: 5,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago E',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 0,
+    },
+    {
+      id: 6,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago F',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 2,
+    },
   ]);
+
   const [filter, setFilter] = useState('all');
 
   const togglePin = (id) => {
@@ -21,13 +71,17 @@ const Chats = () => {
     setChats(chats.map((chat) => (chat.id === id ? { ...chat, muted: !chat.muted } : chat)));
   };
 
+  const markAsRead = (id) => {
+    setChats(chats.map((chat) => (chat.id === id ? { ...chat, newMsg: 0 } : chat)));
+  };
+
   const pinnedChats = chats.filter((chat) => chat.pinned && !chat.muted);
   const recentChats = chats.filter((chat) => !chat.pinned && !chat.muted);
   const mutedChats = chats.filter((chat) => chat.muted);
 
   const filteredChats = (chatsArray) => {
     if (filter === 'notRead') {
-      return chatsArray.filter(chat => chat.newMsg > 0);
+      return chatsArray.filter((chat) => chat.newMsg > 0);
     }
     return chatsArray;
   };
@@ -63,9 +117,9 @@ const Chats = () => {
       <section className="container-barchat">
         <h4 className="sub-title-aside">PINNED CHATS</h4>
         {filteredChats(pinnedChats).map((chat) => (
-          <div className="barchat" key={chat.id}>
+          <div className="barchat" key={chat.id} onClick={() => markAsRead(chat.id)}>
             <div className="profile-img-chat">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
+              <img src={chat.img} alt="" />
             </div>
             <div className="info-chat">
               <span className="name-chat">
@@ -74,7 +128,13 @@ const Chats = () => {
             </div>
             <div className="icons-action-barchat">
               {chat.newMsg > 0 ? <div className="noti-aside">{chat.newMsg}</div> : <div className="noti-aside hidden"></div>}
-              <div className="pin-aside" onClick={() => togglePin(chat.id)}>
+              <div
+                className="pin-aside"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  togglePin(chat.id);
+                }}
+              >
                 <i className="fa-solid fa-thumbtack"></i>
               </div>
             </div>
@@ -82,9 +142,9 @@ const Chats = () => {
         ))}
         <h4 className="sub-title-aside">RECENT CHATS</h4>
         {filteredChats(recentChats).map((chat) => (
-          <div className="barchat" key={chat.id} onMouseEnter={() => setHoveredChatId(chat.id)} onMouseLeave={() => setHoveredChatId(null)}>
+          <div className="barchat" key={chat.id} onClick={() => markAsRead(chat.id)} onMouseEnter={() => setHoveredChatId(chat.id)} onMouseLeave={() => setHoveredChatId(null)}>
             <div className="profile-img-chat">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
+              <img src={chat.img} alt="" />
             </div>
             <div className="info-chat">
               <span className="name-chat">
@@ -96,10 +156,22 @@ const Chats = () => {
             ) : (
               hoveredChatId === chat.id && (
                 <div className="icons-action-barchat">
-                  <div className="action-barchat" onClick={() => toggleMute(chat.id)}>
+                  <div
+                    className="action-barchat"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      toggleMute(chat.id);
+                    }}
+                  >
                     <i className="fa-solid fa-volume-xmark"></i>
                   </div>
-                  <div className="action-barchat" onClick={() => togglePin(chat.id)}>
+                  <div
+                    className="action-barchat"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      togglePin(chat.id);
+                    }}
+                  >
                     <i className="fa-solid fa-thumbtack"></i>
                   </div>
                 </div>
@@ -110,9 +182,9 @@ const Chats = () => {
 
         <h4 className="sub-title-aside">MUTED CHATS</h4>
         {filteredChats(mutedChats).map((chat) => (
-          <div className="barchat" key={chat.id}>
+          <div className="barchat" key={chat.id} onClick={() => markAsRead(chat.id)}>
             <div className="profile-img-chat">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
+              <img src={chat.img} alt="" />
             </div>
             <div className="info-chat">
               <span className="name-chat">
@@ -121,7 +193,13 @@ const Chats = () => {
             </div>
             <div className="icons-action-barchat">
               {chat.newMsg > 0 ? <div className="noti-aside">{chat.newMsg}</div> : <div className="noti-aside hidden"></div>}
-              <div className="pin-aside" onClick={() => toggleMute(chat.id)}>
+              <div
+                className="pin-aside"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleMute(chat.id);
+                }}
+              >
                 <i className="fa-solid fa-volume-xmark"></i>
               </div>
             </div>
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,15 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Contacts = () => {
+  const [chats, setChats] = useState([
+    {
+      id: 1,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago A',
+      info: 'Lorem ipsum dolor sit amet',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: true,
+      muted: false,
+      newMsg: 1,
+    },
+    {
+      id: 2,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago B',
+      info: 'Lorem ipsum dolor sit amet',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: true,
+      newMsg: 3,
+    },
+    {
+      id: 3,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago C',
+      info: 'Lorem ipsum dolor sit amet',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 0,
+    },
+    {
+      id: 4,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago D',
+      info: 'Lorem ipsum dolor sit amet',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 0,
+    },
+    {
+      id: 5,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago E',
+      info: 'Lorem ipsum dolor sit amet',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 0,
+    },
+    {
+      id: 6,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s',
+      name: 'Santiago F',
+      info: 'Lorem ipsum dolor sit amet',
+      message: 'Lorem ipsum dolor sit amet',
+      pinned: false,
+      muted: false,
+      newMsg: 2,
+    },
+  ]);
+
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const toggleSortOrder = () => {
+    const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+    setSortOrder(newSortOrder);
+    sortChats(newSortOrder);
+  };
+
+  const sortChats = (order) => {
+    const sortedChats = [...chats].sort((a, b) => {
+      if (order === 'asc') {
+        return a.name.localeCompare(b.name);
+      } else {
+        return b.name.localeCompare(a.name);
+      }
+    });
+    setChats(sortedChats);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredChats = chats.filter((chat) =>
+    chat.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <aside className="aside">
       <header className="header-chats">
         <h2 className="title-chats">Contacts</h2>
-        <span>
-         <i className="fa-solid fa-arrow-down-a-z"></i>
+        <span onClick={toggleSortOrder} style={{ cursor: 'pointer' }}>
+          <i className={`fa-solid fa-arrow-${sortOrder === 'asc' ? 'down' : 'up'}-a-z`}></i>
         </span>
       </header>
-      
 
       <div className="input-search-container">
         <svg className="icon-input-search" aria-hidden="true" viewBox="0 0 24 24">
@@ -17,134 +107,30 @@ const Contacts = () => {
             <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
           </g>
         </svg>
-        <input placeholder="Search" className="input-search" />
+        <input
+          placeholder="Search"
+          className="input-search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
       </div>
 
       <section className="container-barchat-contacts">
-      <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
+        {filteredChats.map((chat) => (
+          <div className="barchat" key={chat.id}>
+            <div className="profile-img-chat margin-top">
+              <img src={chat.img} alt="" />
+            </div>
+            <div className="info-chat">
+              <span className="info-contact">
+                {chat.name} <span className="content-chat">{chat.info}</span>
+              </span>
+            </div>
           </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
-        <div className="barchat">
-          <div className="profile-img-chat margin-top">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROWl-pf1jCsz-QnUJjwNC3MVgJpDBw10cVqiX2KIEF5g&s" alt="" />
-          </div>
-          <div className="info-chat">
-            <span className="info-contact">
-              {' '}
-              Santiago Espindola <span className="content-chat">Lorem ipsum dolor sit amet </span>
-            </span>
-          </div>
-        </div>
+        ))}
       </section>
-      </aside>
-  )
-}
+    </aside>
+  );
+};
 
-export default Contacts
+export default Contacts;
